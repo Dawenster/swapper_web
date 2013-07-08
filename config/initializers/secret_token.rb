9672a871c06d7319a6cb16dcc3e31493b55bf481
Dawenster <dstwen@gmail.com>
2013-07-08 15:02:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swapper::Application.config.secret_token = '032bb10ba8979667c584069fa362e08e7257c7372fa58fd4c0a7affae38b04c54adab264693b3098efaff19b025626e05358cb3e6f22a6904fb58d0f647190ac'
