1f159b8306ca0ed3aa0832b2822993f9605c85c3
Dawenster <dstwen@gmail.com>
2013-07-09 12:35:28 -0700

    testing code for shifts#create

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -4,7 +4,26 @@ class ShiftsController < ApplicationController
   end
 
   def create
-    
+  	puts "*" * 100
+		puts params["name"]
+    puts params["location"]
+    puts params["locationDetail"]
+    puts params["date"]
+    puts params["duration"]
+    puts params["email"]
+    puts params["taken"]
+    puts params["notes"]
+
+    # shift = Shift.create(:name => params["name"],
+				#     						 :location => params["location"],
+				#     						 :locationDetail => params["locationDetail"],
+				#     						 :date => params["date"],
+				#     						 :duration => params["duration"],
+				#     						 :email => params["email"],
+				#     						 :taken => params["taken"],
+				#     						 :notes => params["notes"])
+
+    render :json => { :id => 1 }
   end
 
   def update
